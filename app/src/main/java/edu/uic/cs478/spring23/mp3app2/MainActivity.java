e18d5efea607c1f1fcd3db0647393d430988e8d2
hamzaali360 <hamali1708@gmail.com>
2023-03-16 20:07:17 -0500

    mp3App2

---
+++ app/src/main/java/edu/uic/cs478/spring23/mp3app2/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.uic.cs478.spring23.mp3app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
